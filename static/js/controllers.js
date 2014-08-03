2ab37625d377e68dd40298f899c7dfe06bcc7314
SHIMIZU Taku <shimizu.taku@gmail.com>
2014-08-03 19:32:39 +0900

    ng-submitの練習をした

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -7,9 +7,11 @@ myAppModule.controller("TextController", function($scope) {
 })
 
 myAppModule.controller("StartUpController", function($scope) {
-    $scope.funding = {startingEstimate: 0};
-
     $scope.computeNeeded = function() {
-        $scope.funding.needed = $scope.funding.startingEstimate * 10;
+        $scope.needed = $scope.startingEstimate * 10;
     };
+
+    $scope.requestFunding = function() {
+        window.alert("もっと顧客を増やしてからにしてください")
+    }
 })
