be16845359045e41523893c122790e86f572f867
SHIMIZU Taku <shimizu.taku@gmail.com>
2014-08-09 16:28:38 +0900

    p29くらいまでやった

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -26,3 +26,26 @@ myAppModule.controller("StudentListController", function($scope) {
         $scope.students.splice(1, 0, {name: "トム・サム", id:4});
     }
 })
+
+myAppModule.controller("AlbumController", function($scope) {
+    var album = [
+        {name: "サウスウェスト・セレナーデ", duration: "2.34"},
+        {name: "ノーザン・ライト・ワルツ", duration: "3:21"},
+        {name: "イースタン・タンゴ", duration: "17.45"}
+    ];
+
+    $scope.album = album;
+})
+
+myAppModule.controller("DeathrayMenuController", function($scope) {
+    $scope.menuState = {show: false};
+    $scope.isDisabled = false;
+
+    $scope.toggleMenu = function() {
+        $scope.menuState.show = !$scope.menuState.show;
+    };
+
+    $scope.stun = function() {
+        $scope.isDisabled = true;
+    }
+})
