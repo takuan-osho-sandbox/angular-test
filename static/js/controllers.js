37f18419518e2b7eaf8127edf78f35ac9d99cb4c
SHIMIZU Taku <shimizu.taku@gmail.com>
2014-08-17 17:10:44 +0900

    p37までやった

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -65,7 +65,7 @@ myAppModule.controller("HeaderController", function($scope) {
         $scope.isWarning = true;
         $scope.isError = false;
     }
-})
+});
 
 myAppModule.controller("RestaurantTableController", function($scope) {
     $scope.directory = [
@@ -77,4 +77,34 @@ myAppModule.controller("RestaurantTableController", function($scope) {
     $scope.selectRestaurant = function(row) {
         $scope.selectedRow = row;
     };
+});
+
+myAppModule.controller("CartController", function($scope) {
+    $scope.bill = {};
+
+    $scope.items = [
+        {title: "ペイントポット", quantity: 8, price: 3.95},
+        {title: "ポルカドット", quantity: 17, price: 12.95},
+        {title: "ペブル", quantity: 5, price: 6.95},
+    ];
+
+    $scope.totalCart = function() {
+        var total = 0;
+        for (var i = 0, len = $scope.items.length; i < len; i++) {
+            total = total + $scope.items[i].price * $scope.items[i].quantity;
+        }
+
+        return total;
+    };
+
+
+    $scope.subtotal = function() {
+        return $scope.totalCart() - $scope.bill.discount;
+    };
+
+    function calculateDiscount(newValue, oldValue, $scope) {
+        $scope.bill.discount = newValue > 100 ? 10 : 0;
+    }
+
+    $scope.$watch($scope.totalCart, calculateDiscount);
 })
